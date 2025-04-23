42f5c1b50bd6797ef477733c58828707215c6b9a
anika-dewari <anikadewari26@gmail.com>
2025-04-24 01:26:01 +0530

    Created user Profile

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,15 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors'); // allows controlled access to resources on a server from a different origin, enabling web applications to make API requests to external services.
 const app = express();
+const cookieParser=require('cookie-parser'); // parses cookies attached to the client request object
 const connectToDb = require('./db/db');
 const userRoutes= require('./routes/user.routes');
 connectToDb();
 
-app.use(cors());
+app.use(cors());    
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use (cookieParser()); // parses the cookies in the request and makes them available in req.cookies
 app.get('/',(req,res)=>{
     res.send("hello world");
 });
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
 const userController = require('../controller/user.controller');
-// const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
+const blacklistTokenmodel = require('../models/blacklistToken.model');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email.'),
@@ -12,5 +13,21 @@ router.post('/register',[
 ],
     userController.registerUser
 )
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email.'),
+    body('password').isLength({min:6}).withMessage('Password must contain more than 6 characters.'),
+],
+    userController.loginUser
+)
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+router.get('/logout', authMiddleware.authUser, async(req, res) => {
+    res.clearCookie('token'); // Clear the cookie
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    await blacklistTokenmodel.create({ token }); // Add the token to the blacklist
+    res.status(200).json({ message: 'Logged out successfully' });
+})
 
 module.exports = router;
